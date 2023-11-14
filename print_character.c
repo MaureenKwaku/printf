24a1f78895afda6a8ceb635d73332aac24a2fdc4
MaureenKwaku <seyramablakwaku@gmail.com>
2023-11-14 14:31:42 -0800

    update the printf file

--- print_character.c
+++ print_character.c
@@ -2,13 +2,14 @@
 
 /**
  * print_char - prints out a char
- * @c: a parameter to be checked
+ * @args: a parameter to be checked
  * Return: 1
 **/
 
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	_putchar(c);
 	return (1);
 }
