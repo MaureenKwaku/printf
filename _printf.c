98f4f363636e84d127c5f737590f46a359e03c8b
Ben_X <benowusuantwi819@gmail.com>
2023-11-14 08:34:28 -0800

    Correction to _printf.c

--- _printf.c
+++ _printf.c
@@ -4,45 +4,46 @@
  * _printf - a function to print out to the standard output
  * @format: a parameter to be checked
  * Return: length
- **/
+**/
 
 int _printf(const char *format, ...)
 {
+	convert p[] = {
+		{"%s", print_string}, {"%c", print_char},
+		{"%%", print_percent},
+		{"%i", print_int}, {"%d", print_decimal},
+		{"%b", print_binary},
+		{"%u", print_unsigned},
+		{"%o", print_octal}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_string}
+	};
+
 	va_list args;
+	int i = 0;
+	int j = 0;
 	int length = 0;
 
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	while (*format)
+Here:
+	while (format[i] != '\0')
 	{
-		if (*format == '%' && format[1] != '\0')
+		j = 10;
+		while (j >= 0)
 		{
-			format++;
-			switch (*format)
+			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
 			{
-				case 'c':
-					length += print_char(va_arg(args, int));
-					break;
-				case 's':
-					length += print_string(va_arg(args, char *));
-					break;
-				case '%':
-					_putchar('%');
-					length++;
-					break;
-				default:
-					_putchar('%');
-					_putchar(*format);
-					length += 2;
-					break;
+				length += p[j].function(args);
+				i = i + 2;
+				goto Here;
 			}
+			j--;
 		}
-		else
-		{
-			_putchar(*format);
-			length++;
-		}
-		format++;
+		_putchar(format[i]);
+		length++;
+		i++;
 	}
 	va_end(args);
 	return (length);
